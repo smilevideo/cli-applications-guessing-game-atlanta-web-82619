8723a68cdd0e71d4984ba31e4e60135a4c1181ba
andrew kim <andrewk1216@gmail.com>
2019-08-22 03:42:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,4 +3,11 @@ def run_guessing_game
   number = rand(1..6)
   puts 'guess 1 to 6, integers'
   check = gets.chomp
+  if check == number
+    puts 'You guessed the correct number!'
+  elsif check == 'exit'
+    puts 'Goodbye!'
+  else
+    puts "Sorry! The computer guessed #{number}."
+  end
 end
